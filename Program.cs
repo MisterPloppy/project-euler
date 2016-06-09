7fdf99c644772f9a1033bb7bd478ef4fade6c612
MisterPloppy <ekirpo@gmail.com>
2016-06-08 21:22:58 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EulerProb1
+{
+    /*
+     *  If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ 
+        Find the sum of all the multiples of 3 or 5 below 1000.
+     * 
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long sumMultiples = 0;
+
+            for (long i = 0; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sumMultiples += i;
+                }
+            }
+
+            Console.WriteLine("Sum of multiples: " + sumMultiples.ToString());
+            Console.ReadLine();
+        }
+    }
+}
